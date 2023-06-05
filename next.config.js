6ef9a1ed3cd2ce2f64d97e7b023df318956b76a8
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2023-06-05 07:26:21 +0200

    Add prettier

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	poweredByHeader: false,
+	env: {
+		APP_URL: process.env.REACT_APP_URL,
+		APP_ENV: process.env.REACT_APP_ENV,
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: `http://localhost:4200/api/:path*`,
+			},
+			{
+				source: '/uploads/:path*',
+				destination: `http://localhost:4200/uploads/:path*`,
+			},
+		]
+	},
 }
 
 module.exports = nextConfig
